b39a5ab2eade757e3dfdd72f6bfdf55ae5fd7b63
hrchlhck <vpemfh7@gmail.com>
2020-05-07 01:15:14 -0300

    Added StateMachine class and refactored code where now the methods are declared inside class

--- main.ino
+++ main.ino
@@ -26,7 +26,7 @@ enum {
     MOVENDO_D,
     ESTACIONADO_D,
     FIM_D
-} estado = INOPERANTE;
+} state = INOPERANTE;
 
 /* Button codes */
 enum {
@@ -80,31 +80,140 @@ void turnOffLED(int pin) {
     digitalWrite(pin, LOW);
 }
 
+class StateMachine {
+    public:
+        void operative() {
+            state = OCIOSO;
+        };
+
+        void inoperative() {
+            state = INOPERANTE;
+        };
+
+        void idle() {
+            state = ALINHADO_S;
+        };
+
+        void lined_up() {
+            state = FIM_S; 
+        };
+
+        void moving_up() {
+            state = ALINHADO_S;
+        };
+
+        void parked_up() {
+            state = ALINHADO_S;
+        };
+
+        void end_up() {
+            state = OCIOSO;
+        };
+
+        void lined_down() {
+            state = ESTACIONADO_D;
+        };
+
+        void moving_down() {
+            state = ALINHADO_D;
+        };
+
+        void parked_down() {
+            state = ALINHADO_D;
+        };
+
+        void end_down() {
+            state = OCIOSO;
+        };
+};
+
 /* Elevator main class */
 class Elevator {
     private:
-        // Attributes
         Adafruit_NeoPixel currentFloorBar;
         Adafruit_NeoPixel destinationBar;
         Adafruit_NeoPixel downBar;
         Adafruit_NeoPixel upBar;
-        bool doorState = false;
+        bool isDoorOpened = false;
         int floorDelay = 1500;
         int currentFloor = 0;
         int embusTime = 2000;
         int floorCount = 10;
         
-        // Methods
-        void changeDestinationBar(int destination);
-        void goDown(int floor);
-        void goUp(int floor);
-        void doorAction();
-        void closeDoor();
-        void openDoor();
+        void changeDestinationBar(int destination) {
+            destinationBar.setPixelColor(currentFloor, 0, 0, 0);
+            destinationBar.setPixelColor(destination, red, green, blue);
+            destinationBar.show();
+        };
+
+        void goDown(int floor) {
+            while(floor < currentFloor - 1) {
+                delay(floorDelay);
+                currentFloor--;
+                currentFloorBar.setPixelColor(currentFloor - 1, red, green, blue);
+                currentFloorBar.show();
+                currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
+            }
+            doorAction();
+        };
+
+        void goUp(int floor) {
+            while(floor >= currentFloor) {
+                delay(floorDelay);
+                currentFloor++;
+                currentFloorBar.setPixelColor(currentFloor - 1, red, green, blue);
+                currentFloorBar.show();
+                currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
+            }
+            doorAction();
+        };      
+
+        void doorAction() {
+            openDoor();
+            delay(embusTime);
+            closeDoor();
+        };
 
     protected:
-        void startPixels();
-        void clearLEDBars();
+        void closeDoor() {
+            isDoorOpened = false;
+        };
+
+        void openDoor() {
+            isDoorOpened = true;
+        };
+
+        void startPixels() {
+            currentFloorBar.begin();
+            currentFloorBar.setPixelColor(currentFloor, red, green, blue);
+            currentFloorBar.show();
+
+            destinationBar.begin();
+            destinationBar.setPixelColor(currentFloor, red, green, blue);
+            destinationBar.show();
+
+            upBar.begin();
+            upBar.setPixelColor(currentFloor, red, green, blue);
+            upBar.show();
+
+            downBar.begin();
+            downBar.setPixelColor(currentFloor, red, green, blue);
+            downBar.show();
+        };
+
+        void clearLEDBars() {
+            currentFloorBar.setPixelColor(currentFloor, 0, 0, 0);
+            currentFloorBar.show();
+
+            destinationBar.setPixelColor(currentFloor, 0, 0, 0);
+            destinationBar.show();
+
+            downBar.setPixelColor(currentFloor, 0, 0, 0);
+            downBar.show();
+
+            upBar.setPixelColor(currentFloor, 0, 0, 0);
+            upBar.show();
+        };
 
     public:
         Elevator(){
@@ -117,169 +226,65 @@ class Elevator {
 
             Serial.println("OK");
         }
-        void move(int floor);
-        bool isDoorOpened();
 
-};
+        void move(int floor) {
+            changeDestinationBar(floor);
+            currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
 
-/* Elevator methods */
-bool Elevator::isDoorOpened() {
-    return doorState;
-};
-
-void Elevator::openDoor() {
-    doorState = true;
-};
-
-void Elevator::closeDoor() {
-    doorState = false;
-};
-
-void Elevator::doorAction() {
-    openDoor();
-    delay(embusTime);
-    closeDoor();
-}
-
-void Elevator::goUp(int floor) {
-    while(floor >= currentFloor) {
-        delay(floorDelay);
-        currentFloor++;
-        currentFloorBar.setPixelColor(currentFloor - 1, red, green, blue);
-        currentFloorBar.show();
-        currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
-    }
-    doorAction();
-};
-
-void Elevator::goDown(int floor) {
-    while(floor < currentFloor - 1) {
-        delay(floorDelay);
-        currentFloor--;
-        currentFloorBar.setPixelColor(currentFloor - 1, red, green, blue);
-        currentFloorBar.show();
-        currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
-    }
-    doorAction();
-};
-
-void Elevator::move(int floor) {
-    changeDestinationBar(floor);
-    currentFloorBar.setPixelColor(currentFloor - 1, 0, 0, 0);
-
-    if(isDoorOpened()) {
-        Serial.println("The door is opened. Please, wait until it closes to proceed.");
-    } else {
-        if(floor > currentFloor) {
-            goUp(floor);
-        } else {
-            goDown(floor);
-        }
-    }
-};
-
-void Elevator::startPixels() {
-    currentFloorBar.begin();
-    currentFloorBar.setPixelColor(currentFloor, red, green, blue);
-    currentFloorBar.show();
-
-    destinationBar.begin();
-    destinationBar.setPixelColor(currentFloor, red, green, blue);
-    destinationBar.show();
-
-    upBar.begin();
-    upBar.setPixelColor(currentFloor, red, green, blue);
-    upBar.show();
-
-    downBar.begin();
-    downBar.setPixelColor(currentFloor, red, green, blue);
-    downBar.show();
-};
-
-void Elevator::changeDestinationBar(int destination) {
-    destinationBar.setPixelColor(currentFloor, 0, 0, 0);
-    destinationBar.setPixelColor(destination, red, green, blue);
-    destinationBar.show();
-};
-
-
-void Elevator::clearLEDBars() {
-    currentFloorBar.setPixelColor(currentFloor, 0, 0, 0);
-    currentFloorBar.show();
-
-    destinationBar.setPixelColor(currentFloor, 0, 0, 0);
-    destinationBar.show();
-
-    downBar.setPixelColor(currentFloor, 0, 0, 0);
-    downBar.show();
-
-    upBar.setPixelColor(currentFloor, 0, 0, 0);
-    upBar.show();
-
+            if(isDoorOpened) {
+                Serial.println("The door is opened. Please, wait until it closes to proceed.");
+            } else {
+                if(floor > currentFloor) {
+                    goUp(floor);
+                } else {
+                    goDown(floor);
+                }
+            }
+        };
 };
 
 class ElevatorController: public Elevator {
     private:
-        bool flagElevator = false;
-        bool isElevatorActive();
+        bool isElevatorActive = false;
     
     public:
-        void move(int floor);
-        void turnOff();
-        void turnOn();
+        void move(int floor) {
+            if(!isElevatorActive) {
+                Serial.println("Unable to perform this action because the elevator is not active");
+            } else {
+                Elevator::move(floor);
+            }
+        };
+        
+        void turnOff() {
+            if(isElevatorActive) {
+                isElevatorActive = false;
+                turnOffLED(elevatorActivatedLEDPin);
+                Serial.println("Deactivating elevator");
+                Elevator::clearLEDBars();
+            } else {
+                Serial.println("Elevator already deactivated");
+            }
+        };
+
+        void turnOn() {
+            if(!isElevatorActive) {
+                isElevatorActive = true;
+                turnOnLED(elevatorActivatedLEDPin);
+                Serial.println("Activating elevator");
+                Elevator::startPixels();
+            } else {
+                Serial.println("Elevator already activated");
+            }
+        };
 };
 
-bool ElevatorController::isElevatorActive() {
-    return flagElevator;
-};
-
-void ElevatorController::turnOn() {
-    if(!flagElevator){
-        flagElevator = true;
-        turnOnLED(elevatorActivatedLEDPin);
-        Serial.println("Activating elevator");
-        Elevator::startPixels();
-    } else {
-        Serial.println("Elevator already activated");
-    }
-};
-
-void ElevatorController::turnOff() {
-    if(flagElevator){
-        flagElevator = false;
-        turnOffLED(elevatorActivatedLEDPin);
-        Serial.println("Deactivating elevator");
-        Elevator::clearLEDBars();
-    } else {
-        Serial.println("Elevator already deactivated");
-    }
-};
-
-void ElevatorController::move(int floor) {
-    if(!isElevatorActive()){
-        Serial.println("Unable to perform this action because the elevator is not active");
-    } else {
-        Elevator::move(floor);
-    }
-};
-
-ElevatorController *elevator;
+StateMachine *state_machine;
 
 /* Elevator moves according to the desired floor by the button pressed */
 void gotoFloor() {
-    int floor = analogRead(A0);
-    switch (floor) {
-        case buttonFloor0: elevator->move(0); break;
-        case buttonFloor1: elevator->move(1); break;
-        case buttonFloor2: elevator->move(2); break;
-        case buttonFloor3: elevator->move(3); break;
-        case buttonFloor4: elevator->move(4); break;
-        case buttonFloor5: elevator->move(5); break;
-        case buttonFloor6: elevator->move(6); break;
-        case buttonFloor7: elevator->move(7); break;
-        case buttonFloor8: elevator->move(8); break;
-        case buttonOff: elevator->turnOff(); break;
-        case buttonOn: elevator->turnOn(); break;
+    int button = analogRead(A0);
+    switch (button) {
     }
 }
 
@@ -291,7 +296,7 @@ void setup() {
 
     attachInterrupt(0, gotoFloor, INPUT_PULLUP);
 
-    elevator = new ElevatorController(); 
+    state_machine = new StateMachine();
 }
 
 void loop() {
